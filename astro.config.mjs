d52d9a604a9269abff42cad4c2f8a5680d4c5f23
Daniel Beeke <mail@danielbeeke.nl>
2023-09-10 17:24:19 +0200

    Removed the old multilangual structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,23 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: 'docs'
+    outDir: 'docs',
+    integrations: [
+        {
+            name: 'gct',
+            hooks: {
+                'astro:config:setup': ({ injectRoute }) => {
+                    injectRoute({
+                        pattern: '/[language]',
+                        entryPoint: './src/pages/index.astro'
+                    })
+
+                    injectRoute({
+                        pattern: '/[language]/[term]',
+                        entryPoint: './src/pages/[term].astro'
+                    })
+                }
+            }
+        }
+    ]
 });
