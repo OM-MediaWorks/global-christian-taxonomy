9d6001e8c3f7002b7d4ff339088bc040d63cd4c9
Daniel Beeke <mail@danielbeeke.nl>
2023-09-10 19:16:21 +0200

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,70 @@
 import { defineConfig } from 'astro/config';
+import temp from 'temp'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+
+temp.track()
+
+const myTemp = temp.mkdirSync('astro')
+
+const hashCode = function(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+  }
 
 // https://astro.build/config
 export default defineConfig({
     outDir: 'docs',
+    resolve: {
+        alias: [
+            { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+        ]
+    },
     integrations: [
         {
             name: 'gct',
             hooks: {
                 'astro:config:setup': ({ injectRoute }) => {
-                    injectRoute({
-                        pattern: '/[language]',
-                        entryPoint: './src/pages/index.astro'
+
+                    const copyInjectRoute = (route) => {
+                        const newEntrypoint = myTemp + '/' + hashCode(route.entryPoint + route.variant) + '.astro'
+                        fs.copyFileSync(route.entryPoint, newEntrypoint)
+                        route.entryPoint = newEntrypoint
+                        injectRoute(route)
+                    }
+
+                    copyInjectRoute({
+                        pattern: '[language]',
+                        entryPoint: 'src/components/index.astro',
+                        variant: 'a'
                     })
 
-                    injectRoute({
-                        pattern: '/[language]/[term]',
-                        entryPoint: './src/pages/[term].astro'
+                    copyInjectRoute({
+                        pattern: '/',
+                        entryPoint: 'src/components/index.astro',
+                        variant: 'b'
+                    })
+
+                    copyInjectRoute({
+                        pattern: '[language]/[term]',
+                        entryPoint: 'src/components/[term].astro',
+                        variant: 'a'
+                    })
+
+                    copyInjectRoute({
+                        pattern: '[term]',
+                        entryPoint: 'src/components/[term].astro',
+                        variant: 'b'
                     })
                 }
             }
